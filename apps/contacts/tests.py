0e52fb37e6216afa743baf33f80980c42018119a
Andrew Shatov <andrii.shatov@gmail.com>
2016-07-15 16:03:34 +0300

    ticket:1 First views test passed, about to start writing a more detailed test with context usage now

--- apps/contacts/tests.py
+++ apps/contacts/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.test import Client
 
+
 class ViewsTestCase(TestCase):
     def setUp(self):
         self.c = Client()
         pass
-
 
     def test_contacts_view(self):
         """
@@ -13,13 +13,6 @@ class ViewsTestCase(TestCase):
         """
         response = self.c.get('/')
         self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'index.html')
-        self.assertEqual(response.method, 'GET')
+        self.assertTemplateUsed(response, 'contacts/index.html')
+        self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
         self.assertEqual(self.c.get('randompagename').status_code, 404)
-
-
-
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 4)
