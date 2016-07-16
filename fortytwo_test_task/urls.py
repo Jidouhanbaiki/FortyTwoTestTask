e642350fd182ed4abf07a7e6f88fa16a40baaf5f
Andrew Shatov <andrii.shatov@gmail.com>
2016-07-16 23:37:14 +0300

    ticket:3 Add a template and modify the old one.

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'contacts.views.index', name='index'),
+    (r'^requests/$', TemplateView.as_view(template_name='requests.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
